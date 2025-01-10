39008c626d0d3a8bd83cd90e3a2e1291d11e5db6
Ninfa-C <ninfa_95@hotmail.it>
2025-01-10 15:06:06 +0100

    directorychange

--- src/components/NetflixNav.jsx
+++ src/components/NetflixNav.jsx
@@ -1,12 +1,12 @@
 import { Component } from "react";
-import 
+
 
 class NetflixNav extends Component{
     render(){
         return(
-            <Container>
+<>
 
-            </Container>
+</>
         )
     }
 }
