da8db3096d1f196acd5a3c5bbd5674b1130d65ac
Ninfa-C <ninfa_95@hotmail.it>
2025-01-10 15:03:05 +0100

    reset

---
+++ Netflix-Clone/src/components/NetflixNav.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import 
+
+class NetflixNav extends Component{
+    render(){
+        return(
+            <Container>
+
+            </Container>
+        )
+    }
+}
+
+export default NetflixNav
